18f1768ad88a1845964254358ee3c3e59aa5206b
oli-lane <markus.oliver.lane@gmail.com>
2024-01-25 15:10:46 -0800

    more routes

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,12 +35,36 @@ const users = {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
 app.get("/users", (req, res) => {
-  res.send(users);
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
