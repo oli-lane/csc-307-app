65dc16c04861456d2129f443a5c8641d3448f28a
oli-lane <markus.oliver.lane@gmail.com>
2024-01-25 15:15:06 -0800

    feat: POST req

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -42,6 +42,11 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
 app.get("/users", (req, res) => {
   const name = req.query.name;
   if (name != undefined) {
@@ -61,6 +66,12 @@ app.get("/users/:id", (req, res) => {
   } else {
     res.send(result);
   }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
 
 app.get("/", (req, res) => {
